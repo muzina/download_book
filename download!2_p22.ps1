7367dd055478b3ca9f81b459741a5efb3b145b24
muzina <meteowave@gmail.com>
2016-07-23 07:48:43 +0900

    Create download!2_p22.ps1

---
+++ download!2_p22.ps1
@@ -0,0 +1,1 @@
+iwr -Uri "http://www.mbga.jp/_game_intro?game_id=12008305" -OutFile test2.html -UserAgent "Mozilla/5.0 (iPad; CPU OS 6_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/6.0 Mobile/10A403 Safari/8536.25"
